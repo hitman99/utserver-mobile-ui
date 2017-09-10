155e486492b34241622f63035239a44038c7a4e9
Tomas <admin@hitman.lt>
2017-09-10 11:02:36 +0300

    WIP - login screen

---
+++ src/react/modules/login.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Header, Grid, Button, Container } from 'semantic-ui-react';
+
+export default class Login extends React.Component {
+    constructor(props){
+        super(props);
+    };
+
+    render() {
+        return(
+            <Container className='v-align'>
+
+            </Container>
+        );
+    };
+}
